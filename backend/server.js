a4d835124029330a5f6f4ba39ed322369d5bd16d
KumarAshutosh99 <formllc11@gmail.com>
2025-05-23 12:15:38 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-const contactRoutes = require('./routes/contactRoutes');
+const contactRoutes = require('./routes/ExampleRoutes');
 app.use('/api/contact', contactRoutes);
 
 
