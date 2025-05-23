e90a6cc6f9c6e266300ed65d33ffa460e787f1f9
KumarAshutosh99 <formllc11@gmail.com>
2025-05-23 12:07:31 +0530

    Your commit message

--- backend/routes/exampleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getExamples, addExample } = require('../controllers/exampleController');
-
-router.get('/', getExamples);
-router.post('/', addExample);
-
-module.exports = router;
